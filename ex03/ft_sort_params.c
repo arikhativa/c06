246bd9353e33258ae764b51e8e80398040844614
Arik Hativa <arikhativa@gmail.com>
2022-05-28 16:10:52 +0300

    ex03

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoav <yoav@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/28 15:30:55 by yoav              #+#    #+#             */
-/*   Updated: 2022/05/28 15:35:30 by yoav             ###   ########.fr       */
+/*   Created: 2022/05/28 15:35:04 by yoav              #+#    #+#             */
+/*   Updated: 2022/05/28 16:09:19 by yoav             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,50 @@ void	ft_putstr(char *s)
 	}
 }
 
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	if (!s1 || !s2)
+		return (0);
+	while (*s1 && *s1 == *s2)
+	{
+		++s1;
+		++s2;
+	}
+	return (*s1 - *s2);
+}
+
 int	main(int ac, char **av)
 {
 	int	i;
+	int	j;
 
-	i = ac - 1;
-	while (i > 0)
+	i = 1;
+	while (i < ac)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+				ft_swap(&av[i], &av[j]);
+			++j;
+		}
+		++i;
+	}
+	i = 1;
+	while (i < ac)
 	{
 		ft_putstr(av[i]);
 		write(1, "\n", 1);
-		--i;
+		++i;
 	}
 	return (0);
 }
